2ac15a2e559aa6e6d2b3bd373418f2e8f0c9cea2
tlauffs <timlauffs@gmail.com>
2025-01-06 18:46:11 +0100

    patch: systray

--- config.h
+++ config.h
@@ -4,7 +4,21 @@
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+
+// sysytray
+/* 0 means no systray */
+static const int showsystray = 1;
+/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 0;
+/* systray spacing */
+static const unsigned int systrayspacing = 2;
+/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systraypinning = 0;
+/* 1: if pinning fails, display systray on the first monitor, False: display *
+ * systray on the last monitor*/
+static const int systraypinningfailfirst = 1;
+
+static const int topbar = 1; /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=12"};
 static const char dmenufont[] = "monospace:size=12";
 static const char col_gray1[] = "#222222";
@@ -23,12 +37,10 @@ static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 // autostart
 static const char *const autostart[] = {
-    "flameshot", NULL,
-    "dunst", NULL,
-    "nm-applet", NULL,
-    "picom", "-b", NULL,
-    "sh", "-c", "feh --randomize --bg-fill ~/wallpapers/*", NULL,
-    NULL /* terminate */
+    "flameshot", NULL, "dunst", NULL,
+    "nm-applet", NULL, "picom", "-b",
+    NULL,        "sh", "-c",    "feh --randomize --bg-fill ~/wallpapers/*",
+    NULL,        NULL /* terminate */
 };
 
 static const Rule rules[] = {
